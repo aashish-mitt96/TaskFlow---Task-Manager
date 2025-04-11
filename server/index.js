287864b2e28e4e5e9913b592a3c31df5863b7c8e
Ashish Mittal <aashishrbmittal@gmail.com>
2025-04-12 00:26:15 +0530

    initial

--- server/index.js
+++ server/index.js
@@ -9,23 +9,24 @@ import taskRoutes from './routes/taskRoutes.js';
 dotenv.config();
 
 const app = express();
+
 app.use(cors({
-  origin: ['https://clickresume.vercel.app', 'http://localhost:5173','http'], 
-  methods: ["POST", "GET", "OPTIONS"], 
-  credentials: true, 
-  allowedHeaders: ["Content-Type", "Authorization", "token"], 
+  origin: ['https://clickresume.vercel.app', 'http://localhost:5173'],
+  methods: ["POST", "GET", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization", "token"],
 }));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-const PORT = process.env.PORT || 5000;
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error(err));
+
+export default app;
